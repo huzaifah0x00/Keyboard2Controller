a5efc374b9707089ba6ee754059df31076138444
huzaifa-007 <huzaifashaikh0012@gmail.com>
2018-08-25 12:54:15 +0500

    bla

--- server.py
+++ server.py
@@ -2,7 +2,8 @@ import pickle
 import socket
 import sys
 from _thread import *
-
+import pyvjoy 
+j = pyvjoy.VJoyDevice(1)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
@@ -11,14 +12,12 @@ try:
 	s.bind((host, port))
 except socket.error as e:
 	print(str(e))
-
+print("Server IP: {}\n Port: {}".format(host,port))
 s.listen(5)
 print('Waiting for a connection.')
 def threaded_client(conn):
 	conn.send(str.encode('Welcome, type your info\n'))
-	i = 1
 	while True:
-		i = i+1 
 		data = conn.recv(2048)
 		if not data:
 			break	  
@@ -27,16 +26,16 @@ def threaded_client(conn):
 				data = pickle.loads(data)
 			except pickle.UnpicklingError:
 				data = "nokeys"
-
 			if data != "nokeys":
 				print(data)
 				#call input function 
 	conn.close()
 
+def inputFunction(data):
+	for i in data:
+		pass
 
 while True:
-
 	conn, addr = s.accept()
 	print('connected to: '+addr[0]+':'+str(addr[1]))
-
 	start_new_thread(threaded_client,(conn,))
