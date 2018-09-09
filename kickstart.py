47f3ab30bffedf83949f4c646f383085ad518eae
huzaifa-007 <huzaifashaikh0012@gmail.com>
2018-09-09 08:26:36 +0500

    bla

--- kickstart.py
+++
@@ -1,5 +0,0 @@
-N = 5 
-K = 1
-cups  = range(1,N+1)
-for today in cups:
-	print(K-today)
--- m.py
+++
@@ -1,9 +0,0 @@
-import time
-import  pyvjoy
-j = pyvjoy.VJoyDevice(1)
-j.set_button(15,1)
-time.sleep(1)
-j.set_button(15,0)
-j.set_button(1,1)
-time.sleep(1)
-j.set_button(1,0)
