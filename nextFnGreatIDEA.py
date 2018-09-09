402ee7ccc9c57a59fd8bb3cb6c8aec21793d62d8
huzaifa-007 <huzaifashaikh0012@gmail.com>
2018-09-09 08:25:23 +0500

    iaks

--- nextFnGreatIDEA.py
+++ nextFnGreatIDEA.py
@@ -1,3 +1,12 @@
 instead Of Streaming Keystrokes continously you could just stream
  their change in state  ( UP or DOWN ) and then as long as its up you don't do anything but when it changes you ' .... YKWIM ( IMPLEMENTED )
-  
+
+
+idea MAKE A GUI FFS 
+	{ 
+		problems = 
+		[
+			IDK GUI yet 
+		]
+		status = "Delayed,"
+	}
