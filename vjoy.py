0a30370b8a90c6e32a0f0230fa979e8b76fb1c85
huzaifah0x00 <huzaifashaikh0012@gmail.com>
2018-11-18 23:41:12 +0500

    hmm

--- vjoy.py
+++ vjoy.py
@@ -4,7 +4,7 @@ from buttonconfig import buttonconfig
 class vJoy:
 	def __init__(self,joystickId):
 	   self.j = pyvjoy.VJoyDevice(joystickId)
-	   BUTTONS = {
+	   self.BUTTONS = {
 	   # 			"Y_AXIS" : self.j.data.wAxisY,
 				# "X_AXIS" : self.j.data.wAxisX,
 				"Y" : 1,
@@ -23,17 +23,20 @@ class vJoy:
 	def handle_buttons(self, keystates):
 		pressedbuttons = [i for i,v in keystates.items() if v]
 		unpressedbuttons = [i for i,v in keystates.items() if not v]
-		for un_button in unpressedbuttons:
-			try:
-				self.j.set_button(buttonconfig[un_button],0)
-			except KeyError:
-				pass
-
 		for p_button in pressedbuttons:
 			try:
-				self.j.set_button(buttonconfig[p_button],1)
+				vj_button = self.BUTTONS[buttonconfig[p_button]]
+				print(f"PRESSING BUTTPNS {vj_button}" )
+				self.j.set_button(vj_button,1)
 			except KeyError:
 				print("No Config defined for this button ({})".format(p_button))
+
+		for un_button in unpressedbuttons:
+			try:
+				vj_button = self.BUTTONS[buttonconfig[un_button]]
+				self.j.set_button(vj_button,0)
+			except KeyError:
+				pass
 
 
 	def handle_movement(self, keystates, wAxisX = 0x4000 , wAxisY = 0x4000):
